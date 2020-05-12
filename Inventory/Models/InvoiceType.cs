512fbf9aaf039de5033c2a54669e9c2786a24ffe
MUHAMMAD RAFAY <MUHAMMAD RAFAY@DESKTOP-6996G2B>
2020-05-13 00:02:31 +0500

    invoice types under development

---
+++ Inventory/Models/InvoiceType.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+namespace Inventory.Models
+{
+    public class InvoiceType
+    {
+        public int id { get; set; }
+        [Required]
+        public string type { get; set; }
+        public void Create()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+        public static List<InvoiceType> get()
+        { return null;
+        }
+        public static DataSet get(int id)
+        {
+            return null;
+        }
+
+    }
+}
